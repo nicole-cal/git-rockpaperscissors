5687657e50798b88f165a2a04072d79a83db3d05
nicole-cal <nicole.cal.73@gmail.com>
2022-04-23 13:39:15 +0100

    Add for loop and get best of 5

--- index.js
+++ index.js
@@ -1,40 +1,67 @@
+let playerScore = 0;
+let computerScore = 0;
+let totalScore = 0;
+
 function computerPlay() {
     const moves = ["rock", "paper", "scissors"];
     const computerSelection = moves[Math.floor(Math.random() * moves.length)];
     return computerSelection;
   }
 
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection.toLowerCase() == computerSelection) {
-            return `You both selected ${playerSelection.toLowerCase()}!`;
-        } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "paper") {
-            return `Computer wins! Paper beats Rock`;
-        } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "scissors") {
-            return `Player wins! Rock beats Scissors`;
-        } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "rock") {
-            return `Player wins! Paper beats Rock`;
-        } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "scissors") {
-            return `Computer wins! Scissors beats Paper`;
-        } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "paper") {
-            return `Player wins! Scissors beats Paper`;
-        } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "rock") {
-            return `Computer wins! Rock beats Scissors`;
-        } else {
-            return `Please enter a valid answer.`
-        }
-    
+let playerWinsRound = `Player wins the round!`;
+let computerWinsRound = `Computer wins the round!`;
+let draw = `It's a draw!`;
+let playerWinsGame = `The player has won the game!`;
+let computerWinsGame = `The computer has won the game!`;
 
+
+function playRound(playerSelection, computerSelection) {
+        if (playerSelection.toLowerCase() == computerSelection) {
+            return draw;
+        } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "paper") {
+           return computerWinsRound;
+        } else if (playerSelection.toLowerCase() == 'rock' && computerSelection == "scissors") {
+           return playerWinsRound;
+        } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "rock") {
+           return playerWinsRound;
+        } else if (playerSelection.toLowerCase() == 'paper' && computerSelection == "scissors") {
+           return computerWinsRound;
+        } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "paper") {
+          return playerWinsRound;
+        } else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == "rock") {
+           return computerWinsRound;
+        } else {
+            return `Please enter a valid answer.`;
+        }
 }
 
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt('Rock, paper, scissors?', '');
+        const computerSelection = computerPlay();
+        let roundResult = playRound(playerSelection, computerSelection);
 
-const playerSelection = 'Rock';
-const computerSelection = computerPlay();
+        if (roundResult === playerWinsRound) {
+            playerScore++;
+            totalScore++;
+        } else if (roundResult === computerWinsRound) {
+            computerScore++;
+            totalScore++;
+        } else if (roundResult === draw) {
+            totalScore++;
+        }
 
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+        console.log(roundResult);
+        console.log(`You chose ${playerSelection}, computer chose ${computerSelection}. Your score is ${playerScore}. Computer's score is ${computerScore}.`);
 
-
+        if (totalScore === 5 && (playerScore > computerScore)) {
+            console.log(playerWinsGame);
+        } else if (totalScore === 5 && (computerScore > playerScore)) {
+            console.log(computerWinsGame);
+        } else if (totalScore === 5 && (computerScore === playerScore)) {
+            console.log(draw);
+        }
+        
+    }
 
 
 
